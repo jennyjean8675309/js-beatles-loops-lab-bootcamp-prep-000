4e5a9e3bb910323f9eed21e939965ed7629bb7ce
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-08-08 16:59:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var beatlesArray = [];
+  for (var n = 0; n <= 3; musicians[n++]){
+    beatlesArray.push(`${musicians[n]} plays ${instruments[n]}`);
+  } return beatlesArray;
+}
+
+function johnLennonFacts(facts){
+  let i = 0;
+  var factsArray = [];
+  while (i < facts.length){
+    factsArray.push(`${facts[i]}!!!`);
+    i++;
+  } return factsArray;
+}
+
+//Because the last index(3) is one less than the length(4), using (i < facts.length) works as the condition.
+
+
+
+
